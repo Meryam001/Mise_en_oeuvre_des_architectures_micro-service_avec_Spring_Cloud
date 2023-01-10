479322a16ace3318c22b5cba01c98c911f44e41f
digital-banking-spring-backend <https://github.com/Meryam001>
2023-01-10 16:46:23 +0100

    first commit

--- customer-account/src/main/java/com/example/customeraccount/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.customeraccount.repository;
-
-import com.example.customeraccount.entities.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface CustomerRepository extends JpaRepository<Customer,Long> {
-}
